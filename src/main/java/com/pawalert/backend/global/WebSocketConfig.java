d76d5645986834e4afa7f92a8805e1684f77d86d
Joojinsung <dev.jinsung1017@gmail.com>
2024-09-02 10:46:58 +0900

    댓글 작성 비밀번호 처리

--- src/main/java/com/pawalert/backend/global/WebSocketConfig.java
+++ src/main/java/com/pawalert/backend/global/WebSocketConfig.java
@@ -12,12 +12,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        // 메시지 브로커 설정
+        config.enableSimpleBroker("/topic");  // 클라이언트로 전송되는 메시지의 브로커가 사용할 경로
+        config.setApplicationDestinationPrefixes("/app"); // 클라이언트가 서버로 메시지를 보낼 때 사용하는 경로
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        // WebSocket 엔드포인트 설정
+        registry.addEndpoint("/ws")  // WebSocket 연결 엔드포인트
+                .setAllowedOrigins("https://port-0-pawalertbackendteamgroup-m06zwfj8628a2164.sel4.cloudtype.app", "http://localhost:8080") // 허용할 출처 설정
+                .withSockJS(); // SockJS 사용
     }
 }
