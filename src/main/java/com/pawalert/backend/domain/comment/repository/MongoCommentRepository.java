2891b16bba2ea5e715f26ef651cf94c202215620
Joojinsung <dev.jinsung1017@gmail.com>
2024-09-07 08:50:33 +0900

    mongodb 수정

--- src/main/java/com/pawalert/backend/domain/comment/repository/MongoCommentRepository.java
+++ src/main/java/com/pawalert/backend/domain/comment/repository/MongoCommentRepository.java
@@ -1,14 +1,14 @@
 package com.pawalert.backend.domain.comment.repository;
 
 import com.pawalert.backend.domain.comment.entity.CommentEntity;
-import com.pawalert.backend.domain.missing.entity.MissingReportEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-
+@Repository
 public interface MongoCommentRepository extends MongoRepository<CommentEntity, String> {
     @Async
     CompletableFuture<List<CommentEntity>> findByMissingReportId(String missingReportId);
