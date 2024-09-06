7122b8cd0b194714c399517746b2b265d5a5f8f7
Joojinsung <dev.jinsung1017@gmail.com>
2024-09-07 08:49:01 +0900

    mongodb 수정

--- src/main/java/com/pawalert/backend/PawAlertBackendApplication.java
+++ src/main/java/com/pawalert/backend/PawAlertBackendApplication.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableMongoRepositories(basePackages = "com.pawalert.backend.domain.notification.repository") // MongoDB 레포지토리 패키지 지정
+@EnableMongoRepositories(basePackages = {
+        "com.pawalert.backend.domain.comment.repository",
+        "com.pawalert.backend.domain.notification.repository"
+})
 public class PawAlertBackendApplication {
 
     public static void main(String[] args) {
