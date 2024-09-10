a5b63ba625d307041048559d5823dac0a3fe0d87
Joojinsung <dev.jinsung1017@gmail.com>
2024-09-10 16:17:41 +0900

    oauth 리다이렉트

--- src/main/java/com/pawalert/backend/global/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/pawalert/backend/global/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -29,37 +29,9 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         System.out.println("OAuth2AuthenticationSuccessHandler: onAuthenticationSuccess called");
 
-        // OAuth2 인증된 사용자 정보 가져오기
-        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-        String authorizationCode = request.getParameter("code"); // 인증 코드 가져오기
-
-        // Google 토큰 엔드포인트 URL 구성
-        String tokenEndpoint = "https://oauth2.googleapis.com/token";
-
-        // 액세스 토큰 요청을 위한 파라미터 구성
-        Map<String, String> tokenRequestParams = new HashMap<>();
-        tokenRequestParams.put("code", authorizationCode);
-        tokenRequestParams.put("client_id", "YOUR_CLIENT_ID");
-        tokenRequestParams.put("client_secret", "YOUR_CLIENT_SECRET");
-        tokenRequestParams.put("redirect_uri", "https://pawalert.co.kr/login/oauth2/code/google");
-        tokenRequestParams.put("grant_type", "authorization_code");
-
-        // 액세스 토큰 요청
-        ResponseEntity<Map> tokenResponse = restTemplate.postForEntity(tokenEndpoint, tokenRequestParams, Map.class);
-        String accessToken = (String) tokenResponse.getBody().get("access_token");
-
-        // 사용자 정보 요청
-        String userInfoEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-
-        ResponseEntity<Map> userInfoResponse = restTemplate.exchange(
-                userInfoEndpoint, HttpMethod.GET, new HttpEntity<>(headers), Map.class);
-        Map<String, Object> userInfo = userInfoResponse.getBody();
-        String email = (String) userInfo.get("email");
-
         // JWT 토큰 생성
-        String jwtToken = jwtTokenProvider.generateToken(email);
+        String jwtToken = jwtTokenProvider.generateToken(authentication.getName());
+
 
         // 응답의 Content-Type을 JSON으로 설정
         response.setContentType("application/json");
