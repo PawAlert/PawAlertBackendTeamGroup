2a046f4060ee262234e3fe2e29eacb2f3bcefd34
Joojinsung <dev.jinsung1017@gmail.com>
2024-09-10 20:13:56 +0900

    oauth 리다이렉트

--- src/main/java/com/pawalert/backend/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/pawalert/backend/global/jwt/CustomUserDetailsService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        UserEntity user = userRepository.findByEmail(email)
+        UserEntity user = userRepository.findByUid(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
         return new CustomUserDetails(user);
     }
