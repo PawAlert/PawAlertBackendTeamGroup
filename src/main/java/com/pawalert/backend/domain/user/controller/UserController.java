61fb0e88bf2812e0c5f63e296c6da33054427b37
devji <dev.jinsung1017@gmail.com>
2024-09-21 20:54:57 +0900

    유저 프로필 수정 로직 수정

--- src/main/java/com/pawalert/backend/domain/user/controller/UserController.java
+++ src/main/java/com/pawalert/backend/domain/user/controller/UserController.java
@@ -33,8 +33,8 @@ public class UserController {
     // 내 정보 업데이트
     @PatchMapping("/update")
     public ResponseEntity<?> updateMyPage(@AuthenticationPrincipal CustomUserDetails user,
-                                          @RequestPart("userUpdateDto") UserUpdateRequest request
-                                         ) {
+                                          @RequestBody UserUpdateRequest request
+    ) {
         return userService.updateMyPage(request, user);
     }
 
