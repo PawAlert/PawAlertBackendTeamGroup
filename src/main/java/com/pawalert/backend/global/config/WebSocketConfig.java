492146186cf22829a7afebd4ea0d79477bce6ec1
devji <dev.jinsung1017@gmail.com>
2024-09-30 23:08:49 +0900

    채팅 기능 서버주소 에러 수정

--- src/main/java/com/pawalert/backend/global/config/WebSocketConfig.java
+++ src/main/java/com/pawalert/backend/global/config/WebSocketConfig.java
@@ -12,18 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // 클라이언트가 구독할 수 있는 경로 설정
-        config.enableSimpleBroker("/queue", "/topic");
-        // 클라이언트에서 서버로 메시지를 보낼 때 사용되는 경로 접두사 설정
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/queue", "/topic"); // 클라이언트가 구독할 경로
+        config.setApplicationDestinationPrefixes("/app"); // 클라이언트가 서버로 보낼 때 경로
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 백엔드 서버에 대한 WebSocket 엔드포인트 설정
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://pawalert.co.kr/")
-                .withSockJS(); // SockJS를 사용하여 WebSocket 연결
+                .setAllowedOrigins("http://localhost:8080", "https://pawalert.co.kr") // 허용할 클라이언트 도메인 추가
+                .withSockJS(); // SockJS 사용
     }
-
 }
